d2ba2b1486edf66cfdae76a0760d89c4c1f07ef3
carp-sushi <105142707+carp-sushi@users.noreply.github.com>
2025-03-12 21:13:20 -0600

    refactoring and naming changes

--- src/Handlers.hs
+++ src/Handlers.hs
@@ -14,9 +14,9 @@ module Handlers
   ) where
 
 import Auth (Account (..))
-import Errors
+import Errors (badRequest, notFound)
 import Models
-import Repo
+import qualified Repo
 
 import Control.Monad.IO.Class (liftIO)
 import Data.String.Conversions (cs)
@@ -24,15 +24,22 @@ import Database.Persist.Sql
 import Servant
 
 -- Get a story from the database.
-getStoryHandler :: ConnectionPool -> Account -> StoryId -> Handler StoryRep
+getStoryHandler ::
+  ConnectionPool ->
+  Account ->
+  StoryId ->
+  Handler StoryRep
 getStoryHandler pool account storyId = do
-  maybeStory <- liftIO $ getStory pool storyId
+  maybeStory <- liftIO $ Repo.getStory pool storyId
   case verify account maybeStory of
     Just story -> return story
     Nothing -> throwError $ notFound "Story not found"
 
 -- Verify story ownership.
-verify :: Account -> Maybe StoryRep -> Maybe StoryRep
+verify ::
+  Account ->
+  Maybe StoryRep ->
+  Maybe StoryRep
 verify _ Nothing = Nothing
 verify account (Just storyRep) =
   if address == accountAddress account
@@ -42,95 +49,146 @@ verify account (Just storyRep) =
     address = cs $ storyAddress_ storyRep
 
 -- Get a page of stories from the database.
-listStoriesHandler :: ConnectionPool -> Account -> Maybe Int -> Maybe Int -> Handler [StoryRep]
+listStoriesHandler ::
+  ConnectionPool ->
+  Account ->
+  Maybe Int ->
+  Maybe Int ->
+  Handler [StoryRep]
 listStoriesHandler pool account maybePage maybeSize = do
   liftIO $
-    listStories pool (cs $ accountAddress account) (getPage maybePage) (getSize maybeSize)
+    Repo.listStories pool address page size
   where
-    getPage Nothing = 1
-    getPage (Just p) = max p 1
-    getSize Nothing = 10
-    getSize (Just s) = max 1 (min s 100)
+    address = cs $ accountAddress account
+    page = getPage maybePage
+    size = getSize maybeSize
+
+-- Get a page number or return a default.
+getPage :: Maybe Int -> Int
+getPage Nothing = 1
+getPage (Just p) = max p 1
+
+-- Get a page size or return a default.
+getSize :: Maybe Int -> Int
+getSize Nothing = 10
+getSize (Just s) = max 1 (min s 100)
 
 -- Delete a story from the database.
-deleteStoryHandler :: ConnectionPool -> Account -> StoryId -> Handler NoContent
+deleteStoryHandler ::
+  ConnectionPool ->
+  Account ->
+  StoryId ->
+  Handler NoContent
 deleteStoryHandler pool account storyId = do
-  maybeStory <- liftIO $ getStory pool storyId
+  maybeStory <- liftIO $ Repo.getStory pool storyId
   case verify account maybeStory of
     Just _ -> do
-      liftIO $ deleteStory pool storyId
+      liftIO $ Repo.deleteStory pool storyId
       return NoContent
     Nothing ->
       throwError $ notFound "Story not found"
 
 -- Validate then insert a story in the database.
-insertStoryHandler :: ConnectionPool -> Account -> StoryReq -> Handler StoryRep
+insertStoryHandler ::
+  ConnectionPool ->
+  Account ->
+  StoryReq ->
+  Handler StoryRep
 insertStoryHandler pool account req =
   if name == ""
     then throwError $ badRequest "Invalid story name"
-    else liftIO $ insertStory pool story
+    else liftIO $ Repo.insertStory pool story
   where
     name = storyReqName req
     address = accountAddress account
     story = Story (cs address) (cs name)
 
 -- Update a story name in the database.
-updateStoryHandler :: ConnectionPool -> Account -> StoryId -> StoryReq -> Handler StoryRep
+updateStoryHandler ::
+  ConnectionPool ->
+  Account ->
+  StoryId ->
+  StoryReq ->
+  Handler StoryRep
 updateStoryHandler pool account storyId storyReq =
-  if name == ""
+  if storyReqName storyReq == ""
     then throwError $ badRequest "Invalid story name"
-    else do
-      maybeStory <- liftIO $ getStory pool storyId
-      case verify account maybeStory of
-        Just _ -> liftIO $ updateStory pool storyId story
-        Nothing -> throwError $ notFound "Story not found"
+    else updateStory' pool account storyId storyReq
+
+-- Update story helper.
+updateStory' ::
+  ConnectionPool ->
+  Account ->
+  StoryId ->
+  StoryReq ->
+  Handler StoryRep
+updateStory' pool account storyId storyReq = do
+  maybeStory <- liftIO $ Repo.getStory pool storyId
+  case verify account maybeStory of
+    Just _ -> liftIO $ Repo.updateStory pool storyId story
+    Nothing -> throwError $ notFound "Story not found"
   where
     name = storyReqName storyReq
     story = Story (cs $ accountAddress account) (cs name)
 
 -- Get tasks for a story from the database.
-listTasksHandler :: ConnectionPool -> Maybe StoryId -> Handler [TaskDto]
-listTasksHandler _ Nothing = throwError $ badRequest "Missing storyId query parameter"
+listTasksHandler ::
+  ConnectionPool ->
+  Maybe StoryId ->
+  Handler [TaskDto]
+listTasksHandler _ Nothing =
+  throwError $ badRequest "Missing storyId query parameter"
 listTasksHandler pool (Just storyId) = do
-  liftIO $ listTasks pool storyId
+  liftIO $ Repo.listTasks pool storyId
 
 -- Get a task from the database.
-getTaskHandler :: ConnectionPool -> TaskId -> Handler TaskDto
+getTaskHandler ::
+  ConnectionPool ->
+  TaskId ->
+  Handler TaskDto
 getTaskHandler pool taskId = do
-  maybeTask <- liftIO $ getTask pool taskId
+  maybeTask <- liftIO $ Repo.getTask pool taskId
   case maybeTask of
     Just task -> return task
     Nothing -> throwError $ notFound "Task not found"
 
 -- Insert a task in the database.
-insertTaskHandler :: ConnectionPool -> Task -> Handler TaskDto
+insertTaskHandler ::
+  ConnectionPool ->
+  Task ->
+  Handler TaskDto
 insertTaskHandler pool task =
   if taskName task == ""
     then throwError $ badRequest "Invalid task name"
     else insertTask' pool task
 
 -- Insert task helper
-insertTask' :: ConnectionPool -> Task -> Handler TaskDto
+insertTask' ::
+  ConnectionPool ->
+  Task ->
+  Handler TaskDto
 insertTask' pool task = do
-  maybeStory <- liftIO $ getStory pool (taskStoryId task)
+  maybeStory <- liftIO $ Repo.getStory pool (taskStoryId task)
   case maybeStory of
+    Just _ -> liftIO $ Repo.insertTask pool task
     Nothing -> throwError $ badRequest "Invalid storyId"
-    Just _ -> liftIO $ insertTask pool task
 
 -- Delete a task from the database.
-deleteTaskHandler :: ConnectionPool -> TaskId -> Handler NoContent
+deleteTaskHandler ::
+  ConnectionPool ->
+  TaskId ->
+  Handler NoContent
 deleteTaskHandler pool taskId = do
-  liftIO $ deleteTask pool taskId
+  liftIO $ Repo.deleteTask pool taskId
   return NoContent
 
 -- Update a task name and status in the database.
-updateTaskHandler :: ConnectionPool -> TaskId -> Task -> Handler TaskDto
+updateTaskHandler ::
+  ConnectionPool ->
+  TaskId ->
+  Task ->
+  Handler TaskDto
 updateTaskHandler pool taskId task = do
   if taskName task == ""
     then throwError $ badRequest "Invalid task name"
-    else updateTask' pool taskId task
-
--- Update task helper
-updateTask' :: ConnectionPool -> TaskId -> Task -> Handler TaskDto
-updateTask' pool taskId task = do
-  liftIO $ updateTask pool taskId task
+    else liftIO $ Repo.updateTask pool taskId task
