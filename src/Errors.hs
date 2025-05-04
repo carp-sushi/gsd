f820f8d087d04fcbdf503dfa39abd3333acc968d
carp-sushi <105142707+carp-sushi@users.noreply.github.com>
2025-05-04 08:46:25 -0600

    reduce code duplication in errors module

--- src/Errors.hs
+++ src/Errors.hs
@@ -6,8 +6,10 @@ module Errors
 where
 
 import Data.Aeson
+import qualified Data.ByteString.Lazy as BS
 import Data.Text (Text)
-import Servant
+import Network.HTTP.Types.Header
+import Servant hiding (Header)
 
 -- Custom error formatter.
 customFormatters :: ErrorFormatters
@@ -18,24 +20,35 @@ customFormatters =
 
 -- Custom JSON formatter for body parser errors.
 bodyParserJsonFormatter :: ErrorFormatter
-bodyParserJsonFormatter _ _ err =
-  err400
-    { errBody = encode $ object ["error" .= err]
-    , errHeaders = [("Content-Type", "application/json")]
-    }
+bodyParserJsonFormatter _ _ = mk400
 
--- Helper for bad request errors.
+-- Helper for creating bad request server errors.
 badRequest :: Text -> ServerError
-badRequest msg =
-  err400
-    { errBody = encode $ object ["error" .= msg]
-    , errHeaders = [("Content-Type", "application/json")]
-    }
+badRequest = mk400
 
--- Helper for not found errors.
+-- Helper for creating not found server errors.
 notFound :: Text -> ServerError
 notFound msg =
   err404
-    { errBody = encode $ object ["error" .= msg]
-    , errHeaders = [("Content-Type", "application/json")]
+    { errBody = jsonError msg
+    , errHeaders = jsonHeader
     }
+
+-- Internal helper. NOTE: Don't know why this can't just be the badRequest function, but changing
+-- it to be so results in call point errors in handlers.
+mk400 :: (ToJSON err) => err -> ServerError
+mk400 err =
+  err400
+    { errBody = jsonError err
+    , errHeaders = jsonHeader
+    }
+
+-- JSON error body
+jsonError :: (ToJSON err) => err -> BS.ByteString
+jsonError err =
+  encode $ object ["error" .= err]
+
+-- JSON error content type headers
+jsonHeader :: [Header]
+jsonHeader =
+  [("Content-Type", "application/json")]
